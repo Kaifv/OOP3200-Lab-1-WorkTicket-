5966963cf1e6469ce64a60f1e0f406e680f558d5
kaif vakil <kaifvakil02@gmail.com>
2020-09-19 16:21:10 -0400

    All the proper changes has been made and appropriate comments are added.

--- WorkTIcket.cpp
+++ WorkTIcket.cpp
@@ -15,6 +15,7 @@
 #include <cmath>			
 #include <limits>			
 #include <stdexcept>		
+#include <utility>
 #include "MyConsoleInput.h" // ConsoleInput::ReadInteger()
 
 using namespace std;
@@ -37,7 +38,7 @@ public:
 	WorkTicket(int myticketNumber = 0, int myticketDay = 1, int myticketMonth = 1, int myticketYear = 2000, string myclientID = "", string myissueDescrip = "");
 
 	// Accesors(getters) - This are the getters function which will be useful in displaying the values of the attributes in the console window. Getter function
-	//for each of the membr of the attribute is added down below.
+	//for each of the member of the attribute is added down below.
 	int GetNumber();
 	int GetDay();
 	int GetMonth();
@@ -52,7 +53,7 @@ public:
 	void SetMonth(int ticketMonth);
 	void SetYear(int ticketYear);
 	void SetID(string clientID);
-	void SetDescription(string issueDescrip);
+	void SetDescription(const string& issueDescrip);
 	bool SetWorkTicket(int ticketNumber, int ticketDay, int ticketMonth, int ticketYear, string clientID, string issueDescrip);
 
 
@@ -83,6 +84,7 @@ int main()
 			inputNumber = ConsoleInput::ReadInteger(1);
 			arr[i].SetNumber(inputNumber);
 
+			// Prompting the user to enter the client ID.
 			std::cout << "Enter the Client ID: ";
 			cin >> inputID;
 			if (inputID.length() >= 1)
@@ -94,18 +96,21 @@ int main()
 				throw invalid_argument("Error! Please enter the ID in the alphanumeric format");
 			}
 
+			// Prompting the user to enter the date of their information. 
 			std::cout << "Enter the date(dd/mm/yyyy): ";
 			cin >> inputday >> inputMonth >> inputYear;
 			arr[i].SetDay(inputday);
 			arr[i].SetMonth(inputMonth);
 			arr[i].SetYear(inputYear);
 
+			// Prompting the user to write the description of their problem. 
 			std::cout << "Write the brief description of the problem: ";
 			cin >> inputDescrip;
 			arr[i].SetDescription(inputDescrip);
 			cout << endl;
 			i++;
 		}
+		// We will catch the exception if any of the input values stands out the validation parameters.
 		catch (exception& ex)
 		{
 			cerr << ex.what();
@@ -114,6 +119,8 @@ int main()
 
 	}
 	std::cout << endl << endl;
+
+	//Now we will show the entered data by the user using a for loop.
 	std::cout << "You entered the noted below information, please check." << endl;
 	for (int y = 0; y < 3; y++)
 	{
@@ -128,24 +135,27 @@ int main()
 
 // class definition section
 
+// Defining constructor with all the necessary parameters. 
 WorkTicket::WorkTicket(int ticketNumber, int ticketDay, int ticketMonth, int ticketYear, string clientID,
 	string issueDescrip)
 {
-	SetWorkTicket(ticketNumber, ticketDay, ticketMonth, ticketYear, clientID, issueDescrip);
+	SetWorkTicket(ticketNumber, ticketDay, ticketMonth, ticketYear, std::move(clientID), std::move(issueDescrip));
 }
 
+// Defining SetWorkTicket function with its parameters and if all are valid then return true.
 bool WorkTicket::SetWorkTicket(int ticketNumber, int ticketDay, int ticketMonth, int ticketYear, string clientID, string issueDescrip)
 {
 	SetNumber(ticketNumber);
 	SetDay(ticketDay);
 	SetMonth(ticketMonth);
 	SetYear(ticketYear);
-	SetID(clientID);
+	SetID(std::move(clientID));
 	SetDescription(issueDescrip);
 
 	return true;
 }
 
+// Defining setter for ticket number which will validate the user input if it is in outside range.
 void WorkTicket::SetNumber(int ticketNumber)
 {
 	if (ticketNumber > 0)
@@ -158,6 +168,7 @@ void WorkTicket::SetNumber(int ticketNumber)
 	}*/
 }
 
+// Defining setter for day attribute in date, so that it is always between 1 and 31.
 void WorkTicket::SetDay(int ticketDay)
 {
 	if (ticketDay >= 1 && ticketDay <= 31)
@@ -170,6 +181,7 @@ void WorkTicket::SetDay(int ticketDay)
 	}
 }
 
+// Defining setter for month attribute in date, so that it is always between 1 and 12.
 void WorkTicket::SetMonth(int ticketMonth)
 {
 	if (ticketMonth >= 1 && ticketMonth <= 12)
@@ -182,6 +194,7 @@ void WorkTicket::SetMonth(int ticketMonth)
 	}
 }
 
+// Defining setter for year attribute in date, so that it is always between 2000 and 2099.
 void WorkTicket::SetYear(int ticketYear)
 {
 	if (ticketYear >= 2000 && ticketYear <= 2099)
@@ -194,16 +207,18 @@ void WorkTicket::SetYear(int ticketYear)
 	}
 }
 
+// Defining setter for client ID, so that it's length is always 1 or more.
 void WorkTicket::SetID(string clientId)
 {
 	/*if (clientId.length() >= 1)
 	{*/
-	myclientID = clientId;
+	myclientID = std::move(clientId);
 	//}
 
 }
 
-void WorkTicket::SetDescription(string issueDescrip)
+// Defining setter for description, so that it's length is always 1 or more.
+void WorkTicket::SetDescription(const string& issueDescrip)
 {
 	if (issueDescrip.length() >= 1)
 	{
@@ -215,37 +230,44 @@ void WorkTicket::SetDescription(string issueDescrip)
 	}*/
 }
 
+// Defining getter for ticket number.
 int WorkTicket::GetNumber()
 {
 	return myticketNumber;
 }
 
+// Defining getter for ticket day. 
 int WorkTicket::GetDay()
 {
 	return myticketDay;
 
 }
 
+// Defining getter for ticket month.
 int WorkTicket::GetMonth()
 {
 	return myticketMonth;
 }
 
+// Defining getter for ticket year. 
 int WorkTicket::GetYear()
 {
 	return myticketYear;
 }
 
+// Defining getter for client ID. 
 string WorkTicket::GetID()
 {
 	return myclientID;
 }
 
+// Defining getter for problem description. 
 string WorkTicket::GetDescription()
 {
 	return myissueDescrip;
 }
 
+// Defining getter to show the data in the proper format. 
 //string WorkTicket::ShowWorkTicket() const
 //{
 //   
